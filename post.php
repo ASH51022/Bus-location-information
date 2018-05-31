ec7d048ab5e390e5e397fc6b0d09c72666ba2043
endo <endo0058.0720@gmail.com>
2018-05-31 09:48:45 +0900

    post

--- post.php
+++ post.php
@@ -1,12 +1,21 @@
 <?php
 
+$ido = "11.2222222"; // ←i☆do
+$keido = "222.3333333"; // ←ke☆ido
 
-$data = http_build_query(array('ido' => '緯度を入れて(はーと)', 'keido' => '経度はこっち(はーと)'), '', '&');
+$data = http_build_query(
+    array(
+        'ido' => $ido,
+        'keido' => $keido
+    ), '', '&'
+);
+
+
 $options = array(
 		'http'=>array(
 				'method' => 'POST',
 				'header' => "Content-type: application/x-www-form-urlencoded\r\n"
-							. "User-Agent: php.file_get_contents\r\n" 
+							. "User-Agent: php.file_get_contents\r\n" // 適当に名乗ったりできます
 							. "Content-Length: " . strlen($data) . "\r\n",
 				'content' => $data
 			)
